fixed gradients for quota system

compare_mechanism/evaluate.py
@@ -52,10 +52,12 @@ def label_valuation(random_bids, allocs, actual_payments, type, args):
 
     elif type == "quota":
         assert args.n_agents > 1, "Quota regularization requires num_agents > 1"
-
+        assert args.n_agents > 1, "Quota regularization requires num_agents > 1"
+        
         allocs = allocs.clamp_min(1e-8)
-        norm_allocs = allocs / allocs.sum(dim=-2).unsqueeze(-1)
-        valuation = torch.tensor([norm_alloc.min().item() for norm_alloc in norm_allocs])
+        norm_allocs = allocs / allocs.sum(dim=-2).unsqueeze(-2)
+
+        valuation =  norm_allocs.min(-1)[0].min(-1)[0]
         optimize = "max"
 
     else:

learnable_preferences/preference/preference.py
@@ -30,9 +30,9 @@ def get_quota(batch, allocs, payments, args):
     with torch.no_grad():
         if args.n_agents > 1:
             allocs = allocs.clamp_min(1e-8)
-            norm_allocs = allocs / allocs.sum(dim=-2).unsqueeze(-1)
-            loss = torch.tensor([norm_alloc.min().item() for norm_alloc in norm_allocs])
+            norm_allocs = allocs / allocs.sum(dim=-2).unsqueeze(-2)
 
+            loss =  norm_allocs.min(-1)[0].min(-1)[0]
             return loss
 
         return torch.zeros(allocs.shape[0]).to(allocs.device)

quota/quota/quota.py
@@ -8,9 +8,9 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 def get_quota(batch, allocs, payments, args):
     if args.quota and args.quota[0] == "quota":
         allocs = allocs.clamp_min(1e-8)
-        norm_allocs = allocs / allocs.sum(dim=-2).unsqueeze(-1)
-        valuation = torch.tensor([norm_alloc.min() for norm_alloc in norm_allocs])
+        norm_allocs = allocs / allocs.sum(dim=-2).unsqueeze(-2)
 
+        valuation =  norm_allocs.min(-1)[0].min(-1)[0]
         loss = (float(args.quota[1]) * valuation).to(DEVICE)
 
         return loss